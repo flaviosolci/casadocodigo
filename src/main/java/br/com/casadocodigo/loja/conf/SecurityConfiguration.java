1a123e3d11c51df35a0f8cb760e6cba9c7adfcfd
Flavio Solci <flaviosolci@gmail.com>
2020-01-18 20:31:01 -0300

    fix: adicionado permissao para relatorio. Funcionalidade 1

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/usuarios/**").hasRole("ADMIN")
 			.antMatchers("/carrinho/**").permitAll()	
 			.antMatchers("/pagamento/**").permitAll()	
+			.antMatchers("/relatorio-produtos/**").permitAll()	
 			.antMatchers("/").permitAll()
 			.antMatchers("/url-magica-maluca-oajksfbvad6584i57j54f9684nvi658efnoewfmnvowefnoeijn").permitAll()
 			.anyRequest().authenticated()
